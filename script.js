cdd1e463f902fe2d0bcf0f533a947ce13e61b854
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-03-08 01:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.video');
+const progressBar = document.querySelector('.progress__filled');
+const playerButton = document.querySelector('.player__button');
+const volumeInput = document.querySelector('.volume');
+const playbackSpeedInput = document.querySelector('.playbackSpeed');
+const rewindButton = document.querySelector('.rewind');
+const forwardButton = document.querySelector('.forward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function updateProgressBar() {
+  const progress = (100 * video.currentTime) / video.duration;
+  progressBar.style.width = progress + '%';
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function togglePlayPause() {
+  if (video.paused) {
+    video.play();
+    playerButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playerButton.textContent = '►';
+  }
+}
+
+function updateVolume() {
+  video.volume = volumeInput.value;
+}
+
+function updatePlaybackSpeed() {
+  video.playbackRate = playbackSpeedInput.value;
+}
+
+function skipBackward() {
+  video.currentTime -= 10;
+}
+
+function skipForward() {
+  video.currentTime += 25;
+}
+
+video.addEventListener('timeupdate', updateProgressBar);
+playerButton.addEventListener('click', togglePlayPause);
+volumeInput.addEventListener('input', updateVolume);
+playbackSpeedInput.addEventListener('input', updatePlaybackSpeed);
+rewindButton.addEventListener('click', skipBackward);
+forwardButton.addEventListener('click', skipForward);
